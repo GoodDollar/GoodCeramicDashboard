c2aa93930ab9ed6fbdba17dcab797c3ef6a62229
John Smith <alexey@gooddollar.org>
2022-04-11 17:10:23 +0300

    add DATABASE_URL support

--- config/database.js
+++ config/database.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const { URL } = require('url');
+const { trimStart } = require('lodash');
+
+const getPostgresCreds = env => {
+  const databaseUrl = env('')
+}
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite')
+  const databaseUrl = env('DATABASE_URL')
+  const { protocol, username, password, host, port, pathname } = databaseUrl ? new URL(databaseUrl) : {}
+  const client = env('DATABASE_CLIENT', protocol || 'sqlite')
 
   const options = {
     postgres: {},
@@ -12,14 +20,14 @@ module.exports = ({ env }) => {
 
   const connectionOptions = {
     sqlite: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'var/db.sqlite3')),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', pathname || 'var/db.sqlite3')),
     },
     postgres: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: env('DATABASE_HOST', host || '127.0.0.1'),
+      port: env.int('DATABASE_PORT', port ? parseInt(port) : 5432),
+      database: env('DATABASE_NAME', trimStart(pathname, '/')),
+      user: env('DATABASE_USERNAME', username),
+      password: env('DATABASE_PASSWORD', password),
       ssl: {
         // For self-signed certificates
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
