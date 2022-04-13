fb3189bfc0d15fccc94949b3ec8cc16b63e23dcb
John Smith <alexey@gooddollar.org>
2022-04-13 19:13:39 +0300

    add custom security settings for AmazonS3 provider only

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,35 @@
-module.exports = ({ env }) => [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.amazonaws.com`],
-          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.amazonaws.com`],
-          upgradeInsecureRequests: null,
+module.exports = ({ env }) => {
+  const bucket = env('AWS_BUCKET')
+  const provider = env('UPLOAD_PROVIDER', 'local')
+  let strapiSecurity ='strapi::security'
+
+  if ('aws-s3' === provider)  {
+    strapiSecurity = {
+      name: strapiSecurity,
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', `https://${bucket}.s3.amazonaws.com`],
+            'media-src': ["'self'", 'data:', 'blob:', `https://${bucket}.s3.amazonaws.com`],
+            upgradeInsecureRequests: null,
+          },
         },
       },
-    },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    }
+  }
+
+  return [
+    'strapi::errors',
+    strapiSecurity,
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+}
