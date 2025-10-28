b4bb170a269b6f56659f1d2ff5ecd4c3e4061e5e
LewisB <lewis@gooddollar.org>
2025-10-29 00:28:28 +0700

    dont allow empty requestOrigin

--- src/plugins/ceramic-feed/server/policies/origin-allowlist.js
+++ src/plugins/ceramic-feed/server/policies/origin-allowlist.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-const { PolicyError } = require('@strapi/utils').errors;
+const { PolicyError } = require('@strapi/utils').errors
 
 const DEFAULT_ALLOWLIST = [
   'http://localhost:3000',
@@ -9,55 +9,54 @@ const DEFAULT_ALLOWLIST = [
   'http://0.0.0.0:1337',
   'https://localhost:3000',
   'https://localhost:1337',
-  'https://127.0.0.1:1337',
-];
+  'https://127.0.0.1:1337'
+]
 
 const getHeader = (ctx, name) => {
-  const headers = ctx.request?.headers;
+  const headers = ctx.request?.headers
   if (!headers) {
-    return undefined;
+    return undefined
   }
 
-  const lower = name.toLowerCase();
-  return headers[lower];
-};
+  const lower = name.toLowerCase()
+  return headers[lower]
+}
 
 const getOriginFromHeaders = ctx => {
-  const origin = getHeader(ctx, 'origin');
+  const origin = getHeader(ctx, 'origin')
   if (origin) {
-    return origin;
+    return origin
   }
 
-  const referer = getHeader(ctx, 'referer');
+  const referer = getHeader(ctx, 'referer')
   if (!referer) {
-    return null;
+    return null
   }
 
   try {
-    const url = new URL(referer);
-    return url.origin;
+    const url = new URL(referer)
+    return url.origin
   } catch {
-    return null;
+    return null
   }
-};
+}
 
 module.exports = (policyContext, policyConfig, { strapi }) => {
   const allowed =
     policyConfig?.origins ??
-    strapi.config.get(
-      'plugin.ceramic-feed.allowedOrigins',
-      DEFAULT_ALLOWLIST
-    );
+    strapi.config.get('plugin.ceramic-feed.allowedOrigins', DEFAULT_ALLOWLIST)
 
-  const requestOrigin = getOriginFromHeaders(policyContext);
+  const requestOrigin = getOriginFromHeaders(policyContext)
 
   if (!requestOrigin) {
-    return true;
+    return false
   }
 
   if (allowed.includes(requestOrigin)) {
-    return true;
+    return true
   }
 
-  throw new PolicyError('Origin not allowed for posts endpoint');
-};
+  throw new PolicyError(
+    `Origin "${requestOrigin}" not allowed for posts endpoint`
+  )
+}
