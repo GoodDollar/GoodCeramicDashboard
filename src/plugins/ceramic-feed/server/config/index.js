6a4f32b75a2b57dbfcdefb01efc8f9bc00d5205c
LewisB <lewis@gooddollar.org>
2025-11-03 13:09:36 +0700

    fix: missed using default origins in ceramic config

--- src/plugins/ceramic-feed/server/config/index.js
+++ src/plugins/ceramic-feed/server/config/index.js
@@ -1,22 +1,30 @@
-'use strict';
+'use strict'
+const { DEFAULT_ALLOWED_ORIGINS } = require('./defaults')
 
 module.exports = {
   default: ({ env }) => ({
-    ceramicNodeURL: env('CERAMIC_NODE_URL', 'https://ceramic-clay.3boxlabs.com'),
-    web3storageGateway: env('WEB3STORAGE_GATEWAY', 'https://ipfsgateway.goodworker.workers.dev'),
-    orbisContext: env('ORBIS_FEED_CONTEXT', 'kjzl6cwe1jw147bfd2hn7f3j2sdsq6708xnb3a217iz1m18a35v25kgxna3s0os'),
+    ceramicNodeURL: env(
+      'CERAMIC_NODE_URL',
+      'https://ceramic-clay.3boxlabs.com'
+    ),
+    web3storageGateway: env(
+      'WEB3STORAGE_GATEWAY',
+      'https://ipfsgateway.goodworker.workers.dev'
+    ),
+    orbisContext: env(
+      'ORBIS_FEED_CONTEXT',
+      'kjzl6cwe1jw147bfd2hn7f3j2sdsq6708xnb3a217iz1m18a35v25kgxna3s0os'
+    ),
     allowedOrigins: env.array('CERAMIC_FEED_ALLOWED_ORIGINS', [
-      'http://localhost:1337',
-      'http://127.0.0.1:1337',
-      'http://0.0.0.0:1337',
-      'http://localhost:3000',
-      'https://localhost:3000',
-      'https://localhost:1337',
-      'https://127.0.0.1:1337',
-    ]),
+      DEFAULT_ALLOWED_ORIGINS
+    ])
   }),
   validator(config) {
-    const mandatoryOptions = ['ceramicNodeURL', 'ceramicDIDSeed', 'web3storageGateway']
+    const mandatoryOptions = [
+      'ceramicNodeURL',
+      'ceramicDIDSeed',
+      'web3storageGateway'
+    ]
 
     mandatoryOptions.forEach(option => {
       if (config[option]) {
@@ -27,5 +35,5 @@ module.exports = {
         `Ceramic connection requires '${option}' option to be set.`
       )
     })
-  },
-};
+  }
+}
