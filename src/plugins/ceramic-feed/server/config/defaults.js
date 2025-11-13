f66b2a1a0626538473d5860e8c37480df6b5aca4
sirpy <hadar@gooddollar.org>
2025-11-13 08:47:59 +0200

    add: allow all origins for feed

--- src/plugins/ceramic-feed/server/config/defaults.js
+++ src/plugins/ceramic-feed/server/config/defaults.js
@@ -1,21 +1,6 @@
 'use strict'
 
-const DEFAULT_ALLOWED_ORIGINS = [
-  'http://localhost:3000',
-  'http://localhost:1337',
-  'http://127.0.0.1:1337',
-  'http://0.0.0.0:1337',
-  'https://localhost:3000',
-  'https://localhost:1337',
-  'https://127.0.0.1:1337',
-  'https://goodwallet.xyz',
-  'https://gooddollar.org',
-  'https://gooddapp.org',
-  'https://goodceramic-dev.herokuapp.com',
-  'https://goodceramic-qa.herokuapp.com/',
-  'https://goodceramic-next.herokuapp.com'
-]
-
+const DEFAULT_ALLOWED_ORIGINS = ['*']
 module.exports = {
   DEFAULT_ALLOWED_ORIGINS
 }
